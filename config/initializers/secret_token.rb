7fd7dd9e79925e392eda1694ce55201d319272e3
claudiu <claudiu.garba@gmail.com>
2014-01-25 22:51:26 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wanatrip::Application.config.secret_key_base = '78d0a1428d8c043a2fd6e4c2ad2621de6b1e8ee6800ab4acb89e5dc41293be4618f0983873d91a5900619fcd49c4097b100fc7b983ad36a48e36f695ad3dec98'
